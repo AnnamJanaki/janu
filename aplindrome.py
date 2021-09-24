ca92febf9206b7aadd6c1930d2e9c1083a1c99c5
AnnamJanaki <janakiannam123@gmail.com>
2021-09-24 20:58:32 +0530

    first commit

---
+++ aplindrome.py
@@ -0,0 +1,10 @@
+
+my_str='malayalam'
+my_str=my_str.casefold()
+
+rev_str=reversed(my_str)
+
+if list(my_str)==list(rev_str):
+    print("the string is palindrome")
+else:
+    print("the string is not a palindrome")
